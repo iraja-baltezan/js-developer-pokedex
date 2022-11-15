31a9f576f9a0301cc0bced5b45ec6e8b39fb64fb
iraja-baltezan <iraja.baltezan@gmail.com>
2022-11-15 19:08:05 -0300

    funções e variáveis renomeadas

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,26 +1,26 @@
 
 const pokeApi = {}
 
-function convertPokeApiDetailToPokemon(pokeDetail) {
+function convertPokeApiPokemonToPokemon(pokemonFromApi) {
     const pokemon = new Pokemon()
-    pokemon.number = pokeDetail.id
-    pokemon.name = pokeDetail.name
+    pokemon.number = pokemonFromApi.id
+    pokemon.name = pokemonFromApi.name
 
-    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const types = pokemonFromApi.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
 
     pokemon.types = types
     pokemon.type = type
 
-    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+    pokemon.photo = pokemonFromApi.sprites.other.dream_world.front_default
 
     return pokemon
 }
 
-pokeApi.getPokemonDetail = (pokemon) => {
+pokeApi.getPokemon = (pokemon) => {
     return fetch(pokemon.url)
         .then((response) => response.json())
-        .then(convertPokeApiDetailToPokemon)
+        .then(convertPokeApiPokemonToPokemon)
 }
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
@@ -29,7 +29,7 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
-        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((pokemons) => pokemons.map(pokeApi.getPokemon))
         .then((detailRequests) => Promise.all(detailRequests))
         .then((pokemonsDetails) => pokemonsDetails)
 }
